bd9b1b4403843092a78572813805af3788466745
Paul-Erik Törrönen <poltsi@poltsi.fi>
2025-08-09 21:06:42 +0300

    Set up minimal test application.yaml Use testcontainers for test database
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- service/src/test/java/fi/poltsi/vempain/file/VempainFileServiceApplicationITC.java
+++ service/src/test/java/fi/poltsi/vempain/file/VempainFileServiceApplicationITC.java
@@ -1,16 +1,27 @@
 package fi.poltsi.vempain.file;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(properties = {
 		"vempain.app.frontend-url=http://localhost:3000",
 		"vempain.original-root-directory=/tmp",
 		"vempain.export-root-directory=/tmp"
 })
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class VempainFileServiceApplicationITC {
+	@Container
+	public static PostgreSQLContainer<?> vempainAdminContainer = new PostgreSQLContainer<>("postgres:latest")
+			.withDatabaseName("vempain_file")
+			.withUsername("test")
+			.withPassword("test");
+
 	@Test
 	void contextLoads() {
 	}
-
 }

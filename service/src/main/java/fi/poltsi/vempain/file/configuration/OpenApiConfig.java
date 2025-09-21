4019ed9a2bf499811604a1daf56ae05d6aecb065
Paul-Erik Törrönen <poltsi@poltsi.fi>
2025-09-21 10:24:35 +0300

    Move all configuration classes to configuration-package
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- service/src/main/java/fi/poltsi/vempain/file/configuration/OpenApiConfig.java
+++ service/src/main/java/fi/poltsi/vempain/file/configuration/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package fi.poltsi.vempain.file;
+package fi.poltsi.vempain.file.configuration;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
